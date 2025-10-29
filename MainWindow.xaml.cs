6763b95011aaefff8590edc057f5249c74076bb4
xckrt <xckrt@github.com>
2025-10-29 10:04:09 +0500

    pr7

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trpo7_voroshilov_pr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Doctor doctor = new Doctor();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = doctor;
+        }
+    }
+}
